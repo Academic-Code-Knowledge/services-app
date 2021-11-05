ce1bb01eb5ff25a1bdc69f38a2004837236d110c
Jessel <jesselcanales@gmail.com>
2021-11-05 03:45:49 -0300

    add view controller

---
+++ app/controllers/dashboard_operador_controller.rb
@@ -0,0 +1,5 @@
+class DashboardOperadorController < ApplicationController
+    before_action :authenticate_user!
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  get 'dashboard_operador/index' => 'dashboard_operador/index'
   root to: 'home#index'
   namespace :admin do
     get 'home/index'
