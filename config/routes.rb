09dd1c4fd663296f6b0189ff14b2d06392a13ccf
daniel <danielandresus@gmail.com>
2021-11-05 22:40:39 -0300

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: 'home#index'
+
   namespace :admin do
     get 'home/index'
     get 'home/index' => 'home#index', :as => :root
