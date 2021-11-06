d4828bcd4a65c0e7c33732ee0e02be2d1588a5c5
SuperAnalizador <89427089+SuperAnalizador@users.noreply.github.com>
2021-11-06 10:04:00 -0300

    home usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_in_path_for(resource)
+        puts resource.role
         if resource.role == 'admin' || resource.role == 'operator'
             admin_root_path
         else
