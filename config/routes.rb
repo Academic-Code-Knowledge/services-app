d4c716fb79c073b2b07675a5ddc1b94449750fa3
jesselcortiz <89053446+jesselcortiz@users.noreply.github.com>
2021-11-08 18:41:05 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-  get 'dashboard_operador/index' => 'dashboard_operador/index'
   root to: 'home#index'
   namespace :admin do
     get 'home/index'
